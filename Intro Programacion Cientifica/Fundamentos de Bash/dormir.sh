c583e9f1bdf49e9fa05cd47197ef0de9738acad8
paula-nj <paula.norniella@outlook.com>
2026-01-28 12:15:47 +0100

    Create dormir.sh

---
+++ Intro Programacion Cientifica/Fundamentos de Bash/dormir.sh
@@ -0,0 +1,6 @@
+# Especificamos que se trata de un script ejecutable que debe ser interpretado con bash
+#!/bin/bash 
+# Comando sleep ejecutado durante 500 segundos (establece segundos por defecto)
+sleep 500
+# Se indica que finaliza la ejecución del script 
+exit
